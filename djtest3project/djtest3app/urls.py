4ca2a71d42fb33ce947ee985eedb207987697b91
aleksey.krasnov <krasnov.80@mail.ru>
2024-06-19 13:44:02 +0300

    setting

--- djtest3project/djtest3app/urls.py
+++ djtest3project/djtest3app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -10,3 +12,6 @@ urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('login/', views.UserLoginView.as_view(), name='login'),
 ]
+
+urlpatterns += (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
