875b77b5d874e792ffd9492e0def06c698069275
aleksey.krasnov <krasnov.80@mail.ru>
2024-06-19 13:05:04 +0300

    path

--- djtest3project/djtest3app/urls.py
+++ djtest3project/djtest3app/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('recipe_category/', views.recipe_category, name='recipe_category'),
+    path('list_recipe/', views.list_recipe, name='list_recipe'),
+    path('add_recipe/', views.add_recipe, name='add_recipe'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
 ]
--- djtest3project/djtest3app/views.py
+++ djtest3project/djtest3app/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 
 def index(request):
-    return HttpResponse('Hello Aleksey 2')
+    return render(request, 'djtest3app/index.html')
+
+
+def recipe_category(request):
+    return render(request, 'djtest3app/recipe_category.html')
+
+
+def list_recipe(request):
+    return render(request, 'djtest3app/list_recipe.html')
+
+
+def add_recipe(request):
+    return render(request, 'djtest3app/add_recipe.html')
+
+
+class UserLogoutView(TemplateView):
+    next_page = 'index'
+
+
+class UserRegisterView(TemplateView):
+    template_name = 'djtest3app/user_register.html'
+
+
+class UserLoginView(TemplateView):
+    """
+    Авторизация на сайте
+    """
+    template_name = 'djtest3app/user_login.html'
